99f398a1eccc28a3e0bf76d084681e4d370ace81
Dillon Dickerson <diltdicker@gmail.com>
2018-02-03 14:28:53 -0700

    added the testJSONFile method

--- Assign3/app/src/main/java/edu/asu/bsse/dtdicker/assign3/MainActivity.java
+++ Assign3/app/src/main/java/edu/asu/bsse/dtdicker/assign3/MainActivity.java
@@ -1,7 +1,12 @@
 package edu.asu.bsse.dtdicker.assign3;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //
+        testJSONFile(this);
+    }
+
+    public boolean testJSONFile(Context context) {
+        try {
+            InputStream jsonFile = context.getAssets().open("places.json");
+            byte[] buf = new byte[jsonFile.available()];
+            jsonFile.read(buf);
+            jsonFile.close();
+            String jsonStr = new String(buf, "UTF-8");
+            Log.d("TESTJSONFILE()", "testJSONFile: " + jsonStr);
+            return true;
+        } catch (IOException e) {
+            Log.i("ERROR", "testJSONFile: unable to load file");
+            e.printStackTrace();
+            return false;
+        }
     }
 }
