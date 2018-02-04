45a7b809b14aa85915b28325efc6d153b39b0113
Dillon Dickerson <diltdicker@gmail.com>
2018-02-04 15:23:54 -0700

    added action bar

--- Assign3/app/src/main/java/edu/asu/bsse/dtdicker/assign3/MainActivity.java
+++ Assign3/app/src/main/java/edu/asu/bsse/dtdicker/assign3/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -17,6 +19,13 @@ public class MainActivity extends AppCompatActivity {
 
         //
         testJSONFile(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_bar, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     public boolean testJSONFile(Context context) {
